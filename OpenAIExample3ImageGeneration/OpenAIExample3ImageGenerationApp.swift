9c1cad5ca490f08887d226d851b491e17e3b8eb8
samtari1 <samtariyang@gmail.com>
2026-03-25 00:34:39 -0400

    Initial Commit

---
+++ OpenAIExample3ImageGeneration/OpenAIExample3ImageGenerationApp.swift
@@ -0,0 +1,17 @@
+//
+//  OpenAIExample3ImageGenerationApp.swift
+//  OpenAIExample3ImageGeneration
+//
+//  Created by Quanpeng Yang on 3/25/26.
+//
+
+import SwiftUI
+
+@main
+struct OpenAIExample3ImageGenerationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ OpenAIExample3ImageGenerationTests/OpenAIExample3ImageGenerationTests.swift
@@ -0,0 +1,17 @@
+//
+//  OpenAIExample3ImageGenerationTests.swift
+//  OpenAIExample3ImageGenerationTests
+//
+//  Created by Quanpeng Yang on 3/25/26.
+//
+
+import Testing
+@testable import OpenAIExample3ImageGeneration
+
+struct OpenAIExample3ImageGenerationTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
